a44c3185818b54ab6928cc94feab07eaa5f09cb6
wuil2004 <williamsmatiassan@gmail.com>
2025-01-12 12:06:09 -0600

    ok

--- main.py
+++ main.py
@@ -26,11 +26,11 @@ app.mount("/notebooks", StaticFiles(directory=UPLOAD_FOLDER), name="notebooks")
 def root():
     """Muestra la página principal."""
     try:
-        with open("index.html", "r", encoding="utf-8") as f:
+        with open("indx.html", "r", encoding="utf-8") as f:
             html_content = f.read()
         return HTMLResponse(content=html_content)
     except FileNotFoundError:
-        raise HTTPException(status_code=404, detail="Archivo index.html no encontrado.")
+        raise HTTPException(status_code=404, detail="Archivo indx.html no encontrado.")
 
 
 @app.post("/upload/")
